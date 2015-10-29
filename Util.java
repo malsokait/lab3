23d7f6823090c669f82172bfc4bbb49bd1f895da
Mohammed Al-Sokait <m.alsokait@gmail.com>
2015-10-29 14:53:18 -0400

    Initial commit

---
+++ Util.java
@@ -0,0 +1,11 @@
+/**
+ * Created by Mohammed on 29/10/2015.
+ */
+public class Util {
+    public static String[] words(String lineContent){
+       return null;
+    }
+    public static int editDistance(String s1, String s2){
+        return 0;
+    }
+}
