9fda8042f02f812df1f0ae6bff2dcc7edf487150
mohammed <m.alsokait@gmail.com>
2015-10-30 15:38:00 -0400

    lineconsumer

--- src/LineConsumer.java
+++ src/LineConsumer.java
@@ -1,4 +1,11 @@
-import java.util.concurrent.BlockingQueue;
+import sun.rmi.runtime.Log;
+
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+
 
 /**
  * Created by Mohammed on 29/10/2015.
@@ -6,9 +13,10 @@ import java.util.concurrent.BlockingQueue;
 public class LineConsumer implements Runnable {
     MessageQueue<Line> q1;
     String outputFileName;
+    ArrayList<Line> lines = new ArrayList<>();
 
-
-    public static final int K = 1;
+    public static final String QUERY_WORD = "TEST";
+    public static final int K = 2;
 
     public LineConsumer(MessageQueue<Line> q1, String outputFileName){
         this.q1 = q1;
@@ -19,9 +27,27 @@ public class LineConsumer implements Runnable {
         while (!q1.isEmpty()){
             try {
                 Line line = (Line) q1.take();
+                if(!line.isEnd()) {
+                    String[] words = Util.words(line.content);
+                    for(int i = 0; i < words.length; i++){
+                        if(Util.editDistance(words[i], QUERY_WORD) <= K){
+                            lines.add(line);
+                            System.out.println("Line number: " + line.lineNumber + ", matched word: " + words[i] + ", Edit distance: " + Util.editDistance(words[i], QUERY_WORD));
+                            i = words.length;
+                        }
+                    }
+
+                }
+                BufferedWriter writer = Files.newBufferedWriter(Paths.get(outputFileName));
+                for(Line outputLine : lines){
+                    writer.write(outputLine.content);
+                    writer.newLine();
+                }
+                writer.close();
 
 
-            } catch (InterruptedException e) {
+
+            } catch (InterruptedException | IOException e) {
                 e.printStackTrace();
             }
         }
